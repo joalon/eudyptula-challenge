1a1a7293bb29de56a248b9c5751199db82c0af9c
Joakim Lönnegren <joakimlonnegren@gmail.com>
2018-09-06 21:33:43 +0200

    Have the skeleton of a misc char device now. Need to flesh out the read, write and open calls

--- 6-MiscDevice/simple_driver.c
+++ 6-MiscDevice/simple_driver.c
@@ -1,20 +1,44 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/fs.h>
+#include <linux/slab.h>
 #include <linux/miscdevice.h>
 
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("A simple device driver");
 MODULE_AUTHOR("Joakim Lönnegren");
 
-struct miscdevice *simple_device;
+struct miscdevice simple_device;
+
+int simple_open(struct inode *i, struct file *f) {
+	pr_debug("simple_driver: open()\n");
+	return 0;
+}
+
+ssize_t simple_write(struct file *f, const char *input, size_t size, loff_t *flags) {
+	return 0;
+}
+
+//ssize_t simple_read(struct file *f, const char *c, size_t size, loff_t *flags) {
+//
+//}
+
+static struct file_operations simple_fops = {
+	.open = simple_open,
+	.owner = THIS_MODULE,
+	.write = simple_write,
+//	.read = simple_read,
+};
 
 static int simple_init(void)
 {
+	int ret;
 
-	simple_device->minor = MISC_DYNAMIC_MINOR;
-	simple_device->name = "simple";
+	simple_device.minor = MISC_DYNAMIC_MINOR;
+	simple_device.name = "simple";
+	simple_device.fops = &simple_fops;
 
-	int ret = misc_register(simple_device);
+	ret = misc_register(&simple_device);
 	if (ret)
 		return ret;
 
@@ -25,7 +49,7 @@ static int simple_init(void)
 
 static void simple_exit(void)
 {
-	misc_unregister(simple_device);
+	misc_deregister(&simple_device);
 	pr_debug("simple_driver: Goodbye, world!\n");
 }
 
